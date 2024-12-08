c03aea854016edf484d4faa7ef362a330ac4fc7f
Belkahla <98918742+7louu@users.noreply.github.com>
2024-12-08 14:25:34 +0100

    setting up the express server and integrating the database

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+//importina l express module
+//importina l connectDB function mel db.js file
+const express = require('express');
+const connectDb = require('./config/db');
+//creatina server express
+const app = express();
+//connectina aala database doub ma yet7all serveur
+connectDb();
+
+//data elli tji mel client tnajem tkoun fi format json 
+//express maynjmch yparsi JSON data into Js scriptable objects 
+//fi wost express fama built-in middleware yaaml l faza hedhy 
+app.use(express.json());
+
+//creatina route bech nchekou ken serveur yemchi
+app.get('/', (req, res) =>{
+    res.send('Server is running ...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log('Server running on ${PORT}');
+});
