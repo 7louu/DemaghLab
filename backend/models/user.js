ea01d926496c2ed3426bc68c315c7154a3842345
Belkahla <98918742+7louu@users.noreply.github.com>
2024-12-08 15:02:09 +0100

    added comments to explain schemas and models for MongoDb

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,8 @@
+//I will explain this once for  all the models
+//MongoDB is a NoSQL database matesta3mlch predefined columns 
+//instead it works with schemas for every model and it will store data according 
+//lel schema adhika
+//steps : import mongoose w baad calli function Schema() w creati model mte3ek 
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
