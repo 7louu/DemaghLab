5c74eb2ea75726e3b54d8bda2450000cd2be9e3e
Belkahla <98918742+7louu@users.noreply.github.com>
2024-12-10 23:34:49 +0100

    updates the server file

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const connectDb = require('./config/db');
 //importina userRoutes
 const userRoutes = require('./routes/userRoutes');
-//importing courseRoutes
+//importina courseRoutes
 const courseRoutes = require('./routes/courseRoutes');
+//importina enrollementRoutes
+const enrollementRoutes = require('./routes/enrollementRoutes');
 //creatina server express
 const app = express();
 //connectina aala database doub ma yet7all serveur
@@ -29,6 +31,8 @@ app.get('/test-db', (req, res)=>{
 app.use('/api/users', userRoutes);
 //ajoutina routes mta3 courses lel server
 app.use('/api/courses', courseRoutes);
+//ajoutina routes mta3 enrollement lel server
+app.use('/api/enrollement', enrollementRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
