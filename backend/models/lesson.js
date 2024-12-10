b64aef7ad765a14dacb51f89899ad6e999acd901
Belkahla <98918742+7louu@users.noreply.github.com>
2024-12-10 23:21:18 +0100

    added instructor field for the lessor schema

--- backend/models/lesson.js
+++ backend/models/lesson.js
@@ -4,6 +4,7 @@ const lessonSchema = mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },  
     course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+    instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Lesson',lessonSchema);
